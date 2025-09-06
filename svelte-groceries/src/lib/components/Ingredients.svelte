0943bb5b9f35e8e1219c42e6ae51386c65adae77
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-09-06 15:11:30 -0400

    Ingredients component

---
+++ svelte-groceries/src/lib/components/Ingredients.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Ingredient } from '../../stores/ingredient';
+	import { RingLoader } from 'svelte-loading-spinners';
+	import Button from './ui/button/button.svelte';
+	import { PlusCircleIcon } from '@lucide/svelte';
+
+	export let ingredients: Ingredient[] | undefined;
+</script>
+
+<ul>
+	{#if ingredients}
+		{#each ingredients as ingredient}
+			<li class="flex flex-row p-4">
+				{ingredient.name}
+				<Button variant="ghost"><PlusCircleIcon /></Button>
+			</li>
+		{/each}
+	{:else}<RingLoader color="#14b8a6" />{/if}
+</ul>

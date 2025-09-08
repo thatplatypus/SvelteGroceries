aec8a9381ea04b1f70ea704becb8f5e2deb050a8
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-09-08 18:51:14 -0400

    Correct favicon Correct service worker to help prevent local caching (i.e. not github url)

--- svelte-groceries/src/routes/+layout.svelte
+++ svelte-groceries/src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/x-icon" href="/SvelteGroceries/favicon.ico" />
 </svelte:head>
 
 <ModeWatcher defaultMode="system" />
